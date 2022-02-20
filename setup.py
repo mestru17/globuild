9a8f62ea0e871756f0879b1cf2e5dba9a9749f8c
Mathias <mestru17@student.aau.dk>
2022-02-20 15:18:42 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="globuild",
+    version="0.1.0",
+    description="An easy and opinionated build automation tool for C projects.",
+    author="Mathias Ruby Estrup",
+    url="https://github.com/mestru17/globuild",
+    py_modules=["globuild"],
+)
